7cd5c96f417090bc5d738c4ed1ca76a01ab779cc
lalyos <lalyos@yahoo.com>
2015-07-17 03:17:19 +0200

    Fix version nuber script

--- generate_new_version.sh
+++ generate_new_version.sh
@@ -3,7 +3,7 @@
 VERSION=$(cat VERSION)
 NEW_VERSION=${VERSION%.*}.$((${VERSION##*.}+1))
 echo $NEW_VERSION > VERSION
-sed -i "s:download/v[^\/]*/:download/vX.X.X/:" Dockerfile
+sed -i "s:download/v[^\/]*/:download/v${NEW_VERSION}/:" Dockerfile
 git add VERSION Dockerfile
 git commit -m "prepare release ${NEW_VERSION}"
 git push origin master
